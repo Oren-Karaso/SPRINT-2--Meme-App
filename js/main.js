30d74923a46ae09315a433b08301f27d51d086ef
Oren Karaso <orenkaraso@gmail.com>
2021-02-10 13:47:42 +0200

    basic design

--- js/main.js
+++ js/main.js
@@ -1,1 +1,72 @@
 'use strict'
+
+function init() {
+    gElCanvas = document.getElementById('my-canvas');
+    gCtx = gElCanvas.getContext('2d');
+    console.log('init -> gCtx', gCtx);
+    drawImg();
+    drawText('asshole', gElCanvas.width / 2, gElCanvas.height - 10);
+}
+
+
+function onSave(ev) {
+    ev.preventDefault();
+
+    var elColor = document.getElementById('color').value;
+    var elOutline = document.getElementById('color-outline').value; 
+
+    updateColor(elColor);
+    updateOutlineColor(elOutline);
+    console.log('shape and color:', gCurrentSahpe, gCurrentColor);
+}
+
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'red'
+    gCtx.fillStyle = 'white'
+    gCtx.font = '40px Impact'
+    gCtx.textAlign = 'center'
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+
+function drawImg() {
+    const img = new Image()
+    img.src = 'sqr-img/1.jpg';
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+    }
+}
+
+
+function downloadCanvas(elLink) {
+    const data = gElCanvas.toDataURL()
+    // console.log('downloadCanvas -> data', data)
+    elLink.href = data
+    elLink.download = 'puki'
+}
+
+
+function clearCanvas() {
+    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
+    // You may clear part of the canvas
+    // gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height/4)
+}
+
+
+function drawImgFromlocal() {
+    const img = new Image()
+    img.src = './img/1.jpg';
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height) //img,x,y,xend,yend
+    }
+}
+
+
+function downloadCanvas(elLink) {
+    const data = gElCanvas.toDataURL()
+    elLink.href = data
+    elLink.download = 'my-img.jpg'
+}
--- js/service.js
+++ js/service.js
@@ -1,1 +1,159 @@
 'use strict'
+
+const gTouchEvs = ['touchstart', 'touchmove', 'touchend'];
+
+let gCurrentSahpe;
+let gCurrentColor;
+let gCurrOutlineColor;
+let gElCanvas;
+let gCtx;
+var gStartPos;
+
+let gGallery = [{id: 1, url: 'sqr-img/1.jpg', keywords: ['speach']}];
+var gKeywords = {'happy': 12,'funny puk': 1} 
+var gMeme = { 
+    selectedImgId: 1, 
+    selectedLineIdx: 0, 
+    lines: [ { txt: 'I never eat Falafel', size: 20, align: 'left', color: 'red' } ] 
+}
+
+function getSelectedImg() {
+
+}
+
+function updateColor(color) {
+    gCurrentColor = color;
+}
+
+function updateOutlineColor(color) {
+    gCurrOutlineColor = color;
+}
+
+function addListeners() {
+    addMouseListeners();
+    addTouchListeners();
+    window.addEventListener('resize', () => {
+        resizeCanvas();
+        renderCanvas();
+    });
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousemove', onMove);
+
+    gElCanvas.addEventListener('mousedown', onDown);
+
+    gElCanvas.addEventListener('mouseup', onUp);
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchmove', onMove);
+
+    gElCanvas.addEventListener('touchstart', onDown);
+
+    gElCanvas.addEventListener('touchend', onUp);
+}
+
+function onDown(ev) {
+    const pos = getEvPos(ev);
+    if (!isShapedClicked(pos)) return;
+    gCircle.isDragging = true;
+    gStartPos = pos;
+    document.body.style.cursor = 'grabbing';
+
+}
+
+function onMove(ev) {
+    if (gCircle.isDragging) {
+        const pos = getEvPos(ev);
+        const dx = pos.x - gStartPos.x;
+        const dy = pos.y - gStartPos.y;
+
+        gCircle.pos.x += dx;
+        gCircle.pos.y += dy;
+
+        gStartPos = pos;
+        renderCanvas();
+        renderShape();
+    }
+}
+
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container');
+    gElCanvas.width = elContainer.offsetWidth;
+    gElCanvas.height = elContainer.offsetHeight;
+}
+
+
+function isShapeClicked(clickedPos) {
+    const { pos } = gCurrentSahpe;
+    const distance = Math.sqrt((pos.x - clickedPos.x) ** 2 + (pos.y - clickedPos.y) ** 2);
+    return distance <= gCurrentSahpe.size;
+    
+}
+
+
+function getEvPos(ev) {
+    var pos = {
+        x: ev.offsetX,
+        y: ev.offsetY
+    };
+
+    if (gTouchEvs.includes(ev.type)) {
+        ev.preventDefault();
+        ev = ev.changedTouches[0];
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop
+        };
+    };
+    return pos;
+}
+
+
+function onUp() {
+    gCurrentSahpe.isDragging = false;
+    document.body.style.cursor = 'grab';
+}
+
+function onMove(ev) {
+    if (gCurrentSahpe.isDragging) {
+        const pos = getEvPos(ev);
+        const dx = pos.x - gStartPos.x;
+        const dy = pos.y - gStartPos.y;
+
+        gCurrentSahpe.pos.x += dx;
+        gCurrentSahpe.pos.y += dy;
+
+        gStartPos = pos;
+        renderCanvas();
+        renderShape();
+    };
+}
+
+function onDown(ev) {
+    const pos = getEvPos(ev);
+    if (!isShapeClicked(pos)) return;
+    gCurrentSahpe.isDragging = true;
+    gStartPos = pos;
+    document.body.style.cursor = 'grabbing';
+
+}
+
+function renderCanvas() {
+    gCtx.fillStyle = "antiquewhite";
+    gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height);
+}
+
+
+function renderShape() {
+    const { pos, color, size } = gCurrentSahpe;
+    switch (gCurrentSahpe.name) {
+        case 'triangle': drawTriangle(pos.x, pos.y);
+            break;
+        case 'rectangle': drawRect(pos.x, pos.y);
+            break;
+        case 'circle': drawArc(pos.x, pos.y);
+    }
+
+}
